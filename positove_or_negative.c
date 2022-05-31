44715e40ac5050df317e281671b00b26da733bf4
D JNANA KUSHAL <101515862+DJNANAKUSHAL@users.noreply.github.com>
2022-05-31 13:36:43 +0000

    .....

--- positove_or_negative.c
+++ positove_or_negative.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-main()
+int main()
 {
 	int a;
 	printf("enter a value");
 	scanf("%d",&a);
 	(a>0)?printf("%d is +ve",a):((a<0)?printf("%d is -ve",a):printf("it is a zero"));
 }
-
+/* enter a value3 
+3 is +ve */
